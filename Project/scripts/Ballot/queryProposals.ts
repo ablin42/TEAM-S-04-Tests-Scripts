372099c2436d64737181585a7149db6e594660b2
ablin <space6fic@gmail.com>
2022-05-15 18:57:24 +0200

    added queryProposal/queryVotingResult scripts

---
+++ Project/scripts/Ballot/queryProposals.ts
@@ -0,0 +1,50 @@
+import { BigNumber, Contract, ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
+// eslint-disable-next-line node/no-missing-import
+import { Ballot } from "../../typechain";
+import { writeLogs } from "../../utils";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  writeLogs("Using address", wallet.address);
+
+  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  writeLogs("Wallet balance", balance);
+
+  if (balance < 0.01) throw new Error("Not enough ether");
+  if (process.argv.length < 3) throw new Error("Ballot address missing");
+  const ballotAddress = process.argv[2];
+  writeLogs("Attaching ballot contract interface to address", ballotAddress);
+
+  if (process.argv.length < 4) throw new Error("Proposal number missing");
+  const PROPOSAL_NUMBER = process.argv[3];
+
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+  const PROPOSAL = await ballotContract.proposals(PROPOSAL_NUMBER);
+  writeLogs("NAME", ethers.utils.parseBytes32String(PROPOSAL.name));
+  writeLogs("VOTE_COUNT", PROPOSAL.voteCount.toString());
+  writeLogs("", "")
+  return;
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
