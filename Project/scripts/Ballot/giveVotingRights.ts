fee10840c8cfb2e9b804cfff1c32d261f0c8dc7e
ablin <space6fic@gmail.com>
2022-05-15 18:56:28 +0200

    added giveVotingRight script

--- Project/scripts/Ballot/giveVotingRights.ts
+++ Project/scripts/Ballot/giveVotingRights.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 // eslint-disable-next-line node/no-missing-import
 import { Ballot } from "../../typechain";
+import { writeLogs } from "../../utils";
 
 // This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
 // Do never expose your keys like this
@@ -14,35 +15,37 @@ async function main() {
     process.env.MNEMONIC && process.env.MNEMONIC.length > 0
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-  console.log(`Using address ${wallet.address}`);
+  writeLogs("Using address", wallet.address);
+
   const provider = ethers.providers.getDefaultProvider("ropsten");
   const signer = wallet.connect(provider);
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));
-  console.log(`Wallet balance ${balance}`);
-  if (balance < 0.01) {
-    throw new Error("Not enough ether");
-  }
+  writeLogs("Wallet balance", balance);
+
+  if (balance < 0.01) throw new Error("Not enough ether");
   if (process.argv.length < 3) throw new Error("Ballot address missing");
   const ballotAddress = process.argv[2];
   if (process.argv.length < 4) throw new Error("Voter address missing");
   const voterAddress = process.argv[3];
-  console.log(
-    `Attaching ballot contract interface to address ${ballotAddress}`
-  );
+  writeLogs("Attaching ballot contract interface to address", ballotAddress);
+
   const ballotContract: Ballot = new Contract(
     ballotAddress,
     ballotJson.abi,
     signer
   ) as Ballot;
+
   const chairpersonAddress = await ballotContract.chairperson();
   if (chairpersonAddress !== signer.address)
     throw new Error("Caller is not the chairperson for this contract");
-  console.log(`Giving right to vote to ${voterAddress}`);
+  writeLogs("Giving right to vote to", voterAddress);
   const tx = await ballotContract.giveRightToVote(voterAddress);
-  console.log("Awaiting confirmations");
+  writeLogs("Awaiting confirmations", {});
   await tx.wait();
-  console.log(`Transaction completed. Hash: ${tx.hash}`);
+  writeLogs("Transaction completed. Hash:", tx.hash);
+  writeLogs("", "")
+  return;
 }
 
 main().catch((error) => {
